58dc49272c5bccbad69f06d875220df5da68c98c
schatzjmae84 <schatzjm84@gmail.com>
2026-03-06 15:44:04 -0600

    Added a sloth thumbnail image to my header, and am working on the bootstrap CSS for the links of my header. The alignment is still not correct, but I'll keep researching and get it aligned right.

--- front-end-react/mental-health-app/vite-project/src/components/common/SlothHeader.jsx
+++ front-end-react/mental-health-app/vite-project/src/components/common/SlothHeader.jsx
@@ -1,18 +1,41 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import sleepySloth from '../../assets/sleepy-sloth.jpg';
+import Image from 'react-bootstrap/Image';
 
 const SlothHeader = () => {
     return (
-        <Navbar expand="lg" className='nav-bar-body'>
+        <Navbar expand="lg" className='bg-body-transparent'>
             <Container>
-                <Navbar.Brand href="#home">The Tranquil Sloth</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#mind">Mind</Nav.Link>
-                    <Nav.Link href="#body">Body</Nav.Link>
-                    <Nav.Link href="#heart">Heart</Nav.Link>
-                    <Nav.Link href="#soul">Soul</Nav.Link>
+                <Navbar.Brand href="#home">
+                    <Image
+                        src={sleepySloth}
+                        roundedCircle
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top rounded-circle"
+                        alt="Sloth Logo"
+                        style={{ objectFit: 'cover', borderRadius: '50%' }}
+                    />
+                    The Tranquil Sloth
+                </Navbar.Brand>
+                <Nav className="justify-content-end" activeKey="/">
+                    <Nav.Item>
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/mind">Mind</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/body">Body</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/heart">Heart</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/soul">Soul</Nav.Link>
+                    </Nav.Item>
                 </Nav>
             </Container>
         </Navbar>
